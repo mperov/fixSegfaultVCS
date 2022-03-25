48e7ab3cd4f4fffc7f70a250e7c436fc92abd835
Maksim Perov <coder@frtk.ru>
2022-03-25 19:17:11 +0300

    patching is added

--- patcher.py
+++ patcher.py
@@ -6,7 +6,17 @@
 #
 
 import sys
-import mmap
+
+BUF_SIZE = 1024
+ORIGIN_BYTES = b'\xe8\xf3\xfd\xff\xff\xff\xc8'
+NEW_BYTES = b'\x90\x90\x90\x90\x90'
+
+def patch(path2lib, addresses):
+    with open(path2lib, 'r+b') as fh:
+        for address in addresses:
+            fh.seek(address)
+            fh.write(NEW_BYTES)
+            print(path2lib + ' is patched by ' + str(NEW_BYTES) + ' on positin - ' + hex(address))
 
 if __name__ == "__main__":
     if len (sys.argv) < 2:
@@ -26,7 +36,27 @@ if __name__ == "__main__":
         sys.exit(-1)
 
     print("library - " + path2lib)
-    #with open(path2lib, 'rb') as fh:
-    #    fh.find(b'\xe8\xf3\xfd\xff\xff\xff\xc8')
-    #    bytes4 = fh.read(4)
-    #    print(bytes4)
+
+    addresses = []
+    address = 0
+    count_match = 0
+    with open(path2lib, 'rb') as fh:
+        while True:
+            buf = fh.read(BUF_SIZE)
+            if buf:
+                for byte in buf:
+                    if byte == ORIGIN_BYTES[count_match]:
+                        count_match += 1
+                        if count_match == len(ORIGIN_BYTES):
+                            print(str(ORIGIN_BYTES) + ' is found!')
+                            addr = address - len(ORIGIN_BYTES) + 1
+                            print('Address is ' + hex(addr))
+                            addresses.append(addr)
+                            count_match = 0
+                    else:
+                        count_match = 0
+                    address += 1
+            else:
+                break
+
+    patch(path2lib, addresses)
